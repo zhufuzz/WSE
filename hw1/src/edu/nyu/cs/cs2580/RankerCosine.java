63b2bd547d1b198cb9d3ae97f6dffeceb08b884b
faithfullie <i.alexialou@gmail.com>
2016-10-04 13:14:06 -0400

    added rankercosine and randerql

--- hw1/src/edu/nyu/cs/cs2580/RankerCosine.java
+++ hw1/src/edu/nyu/cs/cs2580/RankerCosine.java
@@ -1,5 +1,7 @@
 package edu.nyu.cs.cs2580;
 
+import java.util.Collections;
+import java.util.StringJoiner;
 import java.util.Vector;
 
 import edu.nyu.cs.cs2580.QueryHandler.CgiArguments;
@@ -23,6 +25,40 @@ public class RankerCosine extends Ranker {
   public Vector<ScoredDocument> runQuery(Query query, int numResults) {
     Vector<ScoredDocument> all = new Vector<ScoredDocument>();
     // @CS2580: fill in your code here.
-    return all;
+    for (int i = 0; i < _indexer.numDocs(); ++i) {
+      all.add(scoreDocument(query, i));
+    }
+    Collections.sort(all, Collections.reverseOrder());
+    Vector<ScoredDocument> results = new Vector<ScoredDocument>();
+    for (int i = 0; i < all.size() && i < numResults; ++i) {
+      results.add(all.get(i));
+    }
+    return results;
   }
+
+
+  private ScoredDocument scoreDocument(Query query, int did){
+      Document doc = _indexer.getDoc(did);
+      double docNum = _indexer._numDocs;
+      Vector<String> docTokens = ((DocumentFull) doc).getConvertedBodyTokens();
+      Vector<String> queryTokens = query._tokens;
+
+      double nominator = 0.0;
+      double dSum = 0.0;
+      double qSum = 0.0;
+      for(String queryToken:queryTokens){
+          double nk = _indexer.corpusDocFrequencyByTerm(queryToken);
+          double fij = Collections.frequency(docTokens,queryToken);
+          double dij = fij; //(Math.log(fij)+1)*Math.log(docNum/nk);
+          double qj = Collections.frequency(queryTokens,queryToken);
+          nominator+=dij*qj;
+          dSum+=dij*dij;
+          qSum+=qj*qj;
+      }
+      double denominator = Math.sqrt(dSum*qSum);
+      double score = (denominator==0)? 0 : nominator/denominator;
+      return new ScoredDocument(query._query, doc, score);
+
+  }
+
 }
--- hw1/src/edu/nyu/cs/cs2580/RankerQl.java
+++ hw1/src/edu/nyu/cs/cs2580/RankerQl.java
@@ -1,5 +1,6 @@
 package edu.nyu.cs.cs2580;
 
+import java.util.Collections;
 import java.util.Vector;
 
 import edu.nyu.cs.cs2580.QueryHandler.CgiArguments;
@@ -23,6 +24,36 @@ public class RankerQl extends Ranker {
   public Vector<ScoredDocument> runQuery(Query query, int numResults) {
     Vector<ScoredDocument> all = new Vector<ScoredDocument>();
     // @CS2580: fill in your code here.
-    return all;
+    for (int i = 0; i < _indexer.numDocs(); ++i) {
+      all.add(scoreDocument(query, i));
+    }
+    Collections.sort(all, Collections.reverseOrder());
+    Vector<ScoredDocument> results = new Vector<ScoredDocument>();
+    for (int i = 0; i < all.size() && i < numResults; ++i) {
+      results.add(all.get(i));
+    }
+    return results;
   }
+
+
+  private ScoredDocument scoreDocument(Query query, int did){
+    Document doc = _indexer.getDoc(did);
+    double docNum = _indexer._numDocs;
+    Vector<String> docTokens = ((DocumentFull) doc).getConvertedBodyTokens();
+    Vector<String> queryTokens = query._tokens;
+    boolean init = true;
+    double score = 0.0;
+    for(String queryToken: queryTokens){
+      double fqi = Collections.frequency(docTokens,queryToken);
+      double pi = fqi/docTokens.size();
+      if(init){
+        score = pi;
+        init = false;
+      }
+      else
+        score *= score;
+    }
+    return new ScoredDocument(query._query, doc, score);
+  }
+
 }
